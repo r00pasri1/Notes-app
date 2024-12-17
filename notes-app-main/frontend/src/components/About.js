25e4aec070d2a81d0984b549090aae708c409865
r00pasri1 <138258153+r00pasri1@users.noreply.github.com>
2024-12-17 18:42:35 +0530

    Notes app with MongoDB integration

---
+++ notes-app-main/frontend/src/components/About.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+
+export const About = () => (
+  <Fragment>
+    <div className="row">
+      <h3>About</h3>
+      <p>
+    A full-stack web app to manage important notes, featuring:
+
+- CRUD operations
+- MongoDB storage
+- React frontend
+- Search functionality
+- JWT authentication
+
+Built with: React, Node.js, Express.js, MongoDB,Flask and JWT.
+      </p>
+    </div>
+  </Fragment>
+);
